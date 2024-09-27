0d174e8cf095da3a577773a237aa91fd86011d59
mbernard <mbernard@student.42lyon.fr>
2024-09-27 08:24:01 +0200

    added awesome tests to the main

--- ex00/src/main.cpp
+++ ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: mbernard <mbernard@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/06 09:29:57 by mbernard          #+#    #+#             */
-/*   Updated: 2024/09/26 10:41:04 by mbernard         ###   ########.fr       */
+/*   Updated: 2024/09/27 08:22:38 by mbernard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,26 +14,50 @@
 #include <string>
 #include "../includes/whatever.hpp"
 
+class Awesome
+{
+  public:
+    Awesome(void) : _n(0) {}
+    Awesome( int n ) : _n( n ) {}
+    Awesome & operator= (Awesome & a) { _n = a._n; return *this; }
+    bool operator==( Awesome const & rhs ) const { return (this->_n == rhs._n); }
+    bool operator!=( Awesome const & rhs ) const{ return (this->_n != rhs._n); }
+    bool operator>( Awesome const & rhs ) const { return (this->_n > rhs._n); }
+    bool operator<( Awesome const & rhs ) const { return (this->_n < rhs._n); }
+    bool operator>=( Awesome const & rhs ) const { return (this->_n >= rhs._n); }
+    bool operator<=( Awesome const & rhs ) const { return (this->_n <= rhs._n); }
+    int get_n() const { return _n; }
+  private:
+    int _n;
+};
+std::ostream & operator<<(std::ostream & o, const Awesome &a) { o << a.get_n(); return o; }
+
 int main(void) {
-    int a = 42;
-    int b = 24;
-    char aChar = 'a';
-    char bChar = 'b';
-    std::string aString = "aaa";
-    std::string bString = "bbb";
+    int c = 24;
+    int d = 42;
+    char cChar = 'c';
+    char dChar = 'd';
+    std::string cString = "ccc";
+    std::string dString = "ddd";
 
-    swap<int>(a,b);
-    swap<char>(aChar, bChar);
-    ::swap<std::string>(aString, bString);
-    std::cout << "a === " << a << " and b === " << b << std::endl;
-    std::cout << "a === " << aChar << " and b === " << bChar << std::endl;
-    std::cout << "a === " << aString << " and b === " << bString << std::endl;
-    std::cout << "max is " << max<int>(a, b) << std::endl;
-    std::cout << "min is " << min<int>(a, b) << std::endl;
-    std::cout << "max is " << max<char>(aChar, bChar) << std::endl;
-    std::cout << "min is " << min<char>(aChar, bChar) << std::endl;
-    std::cout << "max is " << ::max<std::string>(aString, bString) << std::endl;
-    std::cout << "min is " << ::min<std::string>(aString, bString) << std::endl;
+    swap<int>(c,d);
+    swap<char>(cChar, dChar);
+    ::swap<std::string>(cString, dString);
+    std::cout << "c === " << c << " and d === " << d << std::endl;
+    std::cout << "c === " << cChar << " and d === " << dChar << std::endl;
+    std::cout << "c === " << cString << " and d === " << dString << std::endl;
+    std::cout << "max is " << max<int>(c, d) << std::endl;
+    std::cout << "min is " << min<int>(c, d) << std::endl;
+    std::cout << "max is " << max<char>(cChar, dChar) << std::endl;
+    std::cout << "min is " << min<char>(cChar, dChar) << std::endl;
+    std::cout << "max is " << ::max<std::string>(cString, dString) << std::endl;
+    std::cout << "min is " << ::min<std::string>(cString, dString) << std::endl;
 
+    Awesome a(2), b(4);
+
+    swap(a, b);
+    std::cout << a << " " << b << std::endl;
+    std::cout << max(a, b) << std::endl;
+    std::cout << min(a, b) << std::endl;
     return (0);
 }
